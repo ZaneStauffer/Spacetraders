2acde8551d4d5f6673023f925a951687df531cab
Zane Stauffer <38232438+ZaneStauffer@users.noreply.github.com>
2023-06-12 11:13:54 -0400

    Add docstrings for unwrap()

--- archangel/archangel/script_api.py
+++ archangel/archangel/script_api.py
@@ -10,6 +10,20 @@ from errors import *
 # this can be called with unwrap(response) instead of json.loads(response.content)["data"]
 # -> dict
 def unwrap(response):
+    """Converts a spacetrader API response to a python dict.
+    
+    Usage:
+        ship_dict = unwrap(client.fleet.get_my_ship("ship"))
+    
+    Args:
+        response (Response): The response from the spacetraders API.
+        
+    Returns:
+        result (dict): Dict of the response's contents.
+        
+    Raises:
+        ResponseException(status_code, message): If response status code is not 200 (OK).
+    """
     if response.status_code != 200: # If response code is not 200
         raise ResponseException(response.status_code, response.content) # Raise exception
     return json.loads(response.content)["data"] # Return response as dict
