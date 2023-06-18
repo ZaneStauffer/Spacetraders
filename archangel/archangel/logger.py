4769343353c1157b5a2549df677bd89940201396
ZaneStaufer <stauffer_zane@columbusstate.edu>
2023-06-18 17:02:39 -0400

    Add log option to not print

--- archangel/archangel/logger.py
+++ archangel/archangel/logger.py
@@ -3,8 +3,9 @@ from colorama import Fore, Back, Style
 
 LOG_LOCATION = "logs/"
 
-def log(message, should_save=False):
-    print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()) + "::> " + message)
+def log(message, should_save=False, print_to_console=True):
+    if print_to_console:
+        print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()) + "::> " + message)
     # TODO: if should_save is true then save to log file
     
 def colorize(message, color):
