7d9ec85bd3bda548e82b5507c9d46d39fc862d0c
Zane Stauffer <38232438+ZaneStauffer@users.noreply.github.com>
2023-06-19 11:53:05 -0400

    Add purchase_ship.py docstring

--- scripts/purchase_ship.py
+++ scripts/purchase_ship.py
@@ -1,1 +1,12 @@
-
+"""Purchases a ship at a shipyard. 
+    
+    Usage:
+        run purchase_ship.py ship_type shipyard_symbol
+    Args:
+        ship_type (str): The type of ship to purchase from the shipyard (e.g. SHIP_MINING_DRONE)
+        shipyard_symbol (str): The symbol of the shipyard to purchase from.
+    Returns:
+        ship (dict): The purchased ship if purchase is successful
+    Raises:
+      ResponseException(status_code, message)
+"""
