5be060a1649ed6e4c478467faaa9b0312b2b65c9
ZaneStaufer <stauffer_zane@columbusstate.edu>
2023-06-20 22:35:52 -0400

    add get_contracts fixme

--- scripts/get_contracts.py
+++ scripts/get_contracts.py
@@ -6,6 +6,7 @@
     Returns:
         result (dict): Dict of the agent's contracts.
 """
+#FIXME: Return contract model
 try:
     result = unwrap(client.contracts.get_contracts()) # Get the agent's contracts
 except ResponseException as e:
